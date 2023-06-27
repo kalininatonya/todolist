c716a5704d04611a01cec538c48c603e1774dd84
Kalinina Antonina <staraya.kalinina@yandex.ru>
2023-06-27 20:42:43 +0500

    added constants

---
+++ client/src/constant.js
@@ -0,0 +1,15 @@
+export const warnings = {
+    requiredField: 'Обязательно для заполнения',
+    processError: 'Уууупс...Сервер временно недоступен, попробуйте позже.',
+};
+
+export const options = [
+    {text: 'Низкий', value: 'small', priority: 10},
+    {text: 'Средний', value: 'middle', priority: 50},
+    {text: 'Высокий', value: 'high', priority: 100},
+];
+
+export const sort = {
+    'asc': 'ascending', //По возрастанию
+    'desc': 'descending' //По убыванию
+}
